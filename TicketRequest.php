0ed6eb287d293866440871d3eb3c1b3b660e8ce7
nshgt0 <nsght0@gmail.com>
2014-05-17 11:33:22 +0300

    TicketRequest.php

---
+++ TicketRequest.php
@@ -0,0 +1,9 @@
+<?php
+require_once ('lib/nusoap/nusoap.php'); 
+//Δημιουργία soap client
+	$client = new nusoap_client('http://pt-et.info/TicketServer.php'); 
+//$param = η μεταβλητή στην οποία έχουμε αποθηκευμένες τις παραμέτρους που
+//θέλουμε να περάσουμε στην getTicket()
+	$ticketText = $client->call('getTicket',$param); 
+//$ticketText = το κρυπτογραφήμενο κείμενο του εισιτηρίου	
+?>
